b5f084f2f4da1bfdf21abdebfa4b927b6e5faace
Xerefrux <raiyansayeed0@gmail.com>
2024-11-16 00:58:19 +0600

    first commit

---
+++ Week4/Week4/DTOs/CustomerDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week4.DTOs
+{
+    public class CustomerDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public System.DateTime DateJoined { get; set; }
+
+
+    }
+}
---
+++ Week4/Week4/DTOs/ProductDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week4.DTOs
+{
+    public class ProductDTO
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int StockQuantity { get; set; }
+        public string Category { get; set; }
+    }
+}
